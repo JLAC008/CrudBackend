3cd04d5f51664da617821b9e67b4907c472ef7dc
JLAC\josel <joseluji223@gmail.com>
2025-06-28 00:08:47 +0200

    Proyecto

---
+++ CrudJLAC/src/test/java/com/CrudJLAC/CrudJlacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CrudJLAC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJlacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
